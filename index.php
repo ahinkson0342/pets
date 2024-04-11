81c4298382a6e5cc0575ec0b17b5f8b40db29fe5
Andrew Hinkson <adh1056@comcast.net>
2024-04-11 14:23:36 -0400

    added more styling, made the hyperlink an actual hyperlink

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-----------------------Github repo: https://github.com/ahinkson0342/pets---------------------->
 <?php
 
 //turning on error reporting
@@ -13,7 +14,11 @@ $f3 = Base::instance();
 //Define a default route
 $f3->route('GET /',function()
 {
-    echo '<h1>Hello World!</h1>';
+    //echo '<h1>Hello World!</h1>';
+
+    //Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run fat-free
